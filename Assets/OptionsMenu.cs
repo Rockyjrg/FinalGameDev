1f9bff9b00fc71db572857a8cc1744323f99c052
Jamie <jamie.g.ii@hotmail.com>
2024-12-07 05:46:11 -0600

    Added main menu with options section and going to start working on little level
    Need to still implement a way to pause the game and checkpoints.

---
+++ Assets/OptionsMenu.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject mainMenuPanel;
+    [SerializeField] private GameObject optionsPanel;
+    [SerializeField] private Slider volumeSlider;
+    [SerializeField] private Toggle fullscreenToggle;
+    [SerializeField] private Dropdown resolutionDropdown;
+
+    private Resolution[] resolutions;
+
+    private void Start()
+    {
+        // Initialize resolution dropdown
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        foreach (Resolution res in resolutions)
+        {
+            resolutionDropdown.options.Add(new Dropdown.OptionData(res.width + " x " + res.height));
+        }
+
+        resolutionDropdown.value = resolutions.Length - 1;
+        resolutionDropdown.RefreshShownValue();
+
+        // Load saved settings (optional)
+        volumeSlider.value = PlayerPrefs.GetFloat("Volume", 1f);
+        fullscreenToggle.isOn = Screen.fullScreen;
+        resolutionDropdown.value = PlayerPrefs.GetInt("Resolution", resolutions.Length - 1);
+    }
+
+    public void OpenOptions()
+    {
+        mainMenuPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+        optionsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        PlayerPrefs.SetFloat("Volume", volume);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution res = resolutions[resolutionIndex];
+        Screen.SetResolution(res.width, res.height, Screen.fullScreen);
+        PlayerPrefs.SetInt("Resolution", resolutionIndex);
+    }
+}
